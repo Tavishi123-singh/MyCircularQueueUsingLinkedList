84074f743cc55767d87d3848aed48cf59359cde1
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-30 00:35:47 +0530

    Final Commit

--- src/circularQueue/MyCircularQueueUsingLinkedList.java
+++ src/circularQueue/MyCircularQueueUsingLinkedList.java
@@ -4,5 +4,29 @@
  */
 package circularQueue;
 
+import node.Node;
+
 public class MyCircularQueueUsingLinkedList {
+    private Node rear;
+    private int size;
+    public void enqueue(int data) {
+        Node node = new Node(data);
+        if (isEmpty()) {
+            rear = node;
+            size++;
+            node.setNext(node);
+        } else {
+            node.setNext(rear.getNext());
+            rear.setNext(node);
+            rear = node;
+            size++;
+
+        }
+    }
+
+    private boolean isEmpty() {
+        boolean res=false;
+        if(rear==null) res=true;
+        return res;
+    }
 }
