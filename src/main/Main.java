b62e696e5fff5f2169831d462112845d05477fd7
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-30 00:48:43 +0530

    Final Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@
 package main;
 
 import circularQueue.MyCircularQueueUsingLinkedList;
+import node.Node;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +15,17 @@ public class Main {
         queue.enqueue(3000);
         queue.enqueue(4000);
         queue.enqueue(5000);
-
+        Node peek=queue.peek();
+        if(peek!=null){
+            System.out.println(peek.getData() + " is the element at front of the queue");
+        }
+        Node delete=queue.dequeue();
+        if(delete!=null){
+            System.out.println(delete.getData() + " is the element deleted from front");
+        }
+        peek=queue.peek();
+        if(peek!=null){
+            System.out.println(peek.getData() + " is the element at front of the queue");
+        }
     }
 }
