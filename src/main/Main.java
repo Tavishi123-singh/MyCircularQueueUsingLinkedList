15c46518fd9a76d0a5d384150268fbdd9f042763
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-30 00:43:29 +0530

    Final Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,16 @@
  */
 package main;
 
+import circularQueue.MyCircularQueueUsingLinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        MyCircularQueueUsingLinkedList queue=new MyCircularQueueUsingLinkedList();
+        queue.enqueue(1000);
+        queue.enqueue(2000);
+        queue.enqueue(3000);
+        queue.enqueue(4000);
+        queue.enqueue(5000);
+
+    }
 }
